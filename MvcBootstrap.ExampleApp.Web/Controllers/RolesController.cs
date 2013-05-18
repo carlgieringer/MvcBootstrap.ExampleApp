741f75621355e2f4b0ff7a85ca53fd7a55034225
Carl Gieringer <carl@carlgieringer.com>
2013-05-18 12:32:09 -0400

    Map employees in RolesController

--- MvcBootstrap.ExampleApp.Web/Controllers/RolesController.cs
+++ MvcBootstrap.ExampleApp.Web/Controllers/RolesController.cs
@@ -4,12 +4,15 @@
     using MvcBootstrap.Data;
     using MvcBootstrap.ExampleApp.Domain.Models;
     using MvcBootstrap.ExampleApp.Web.ViewModels;
+    using MvcBootstrap.Mapping;
 
     public class RolesController : BootstrapControllerBase<Role ,RoleViewModel>
     {
         public RolesController(IBootstrapRepository<Role> repository)
             : base(repository)
         {
+            MappingHelper.CreateEntityToViewModelMap<Employee, EmployeeViewModel>();
+            MappingHelper.CreateViewModelToEntityMap<EmployeeViewModel, Employee>();
         }
     }
 }
